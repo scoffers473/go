a58c0fd9ce1f643bc37f570b90105b82af203ac6
Chris Scoffield <chrisdscoffield@gmail.com>
2020-09-23 23:16:03 +0100

    add raw strings

--- declarations.go
+++ declarations.go
@@ -4,11 +4,11 @@ import "fmt"
 
 var y = 42
 var z string = "Hello Chris"
+var a = `Chris said "Hello ..."` // Use backquotes for raw strings
 func main() {
 
 	x := 42 // Use := for first declaration and assignment
 	fmt.Println(x)
 	x = 99 // No need for the colon as already been decalred
-	fmt.Println(y)
-	fmt.Printf("Variable %s is of type %T\n",z,z)
+	fmt.Println(a)
 }
