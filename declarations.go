8189b2851a2c35185368c502f59bd24d7c25e6f8
Chris Scoffield <chrisdscoffield@gmail.com>
2020-09-23 23:53:18 +0100

    Final file

--- declarations.go
+++ declarations.go
@@ -3,12 +3,17 @@ package main
 import "fmt"
 
 var y = 42
-var z string = "Hello Chris"
-var a = `Chris said "Hello ..."` // Use backquotes for raw strings
+
 func main() {
 
-	x := 42 // Use := for first declaration and assignment
-	fmt.Println(x)
-	x = 99 // No need for the colon as already been decalred
-	fmt.Println(a)
+	fmt.Println(y)
+	fmt.Printf("Type %T\n", y)                 // Type
+	fmt.Printf("Binary %b\n", y)               // In Binary
+	fmt.Printf("Octal %o\n", y)                // In Octal
+	fmt.Printf("Hex %x\n", y)                  // In Hex
+	fmt.Printf("Hex with leading 0x %#x\n", y) // Hex with leading 0x
+	fmt.Printf("And now altogether %v %b %o %x %#x\n", y, y, y, y, y)
+
+	s := fmt.Sprintf("And now altogether %v %b %o %x %#x\n", y, y, y, y, y) // Use sprintf to create formatted string
+	fmt.Println(s)
 }
