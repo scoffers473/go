1f2f378e6c7dc06621e8ada83fa7b600946eadfa
Chris Scoffield <chrisdscoffield@gmail.com>
2020-09-23 23:08:21 +0100

    playing with printf

--- declarations.go
+++ declarations.go
@@ -2,16 +2,13 @@ package main
 
 import "fmt"
 
-var outside = "global var" // Use var when defining outside a func
-var z int // Declare z as int and assigns a ZERO alue
+var y = 42
+var z string = "Hello Chris"
 func main() {
 
 	x := 42 // Use := for first declaration and assignment
 	fmt.Println(x)
 	x = 99 // No need for the colon as already been decalred
-	fmt.Println(x)
-	y := 100 + 70
 	fmt.Println(y)
-	fmt.Println(z)
-	fmt.Println(outside)
+	fmt.Printf("Variable %s is of type %T\n",z,z)
 }
