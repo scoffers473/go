75c721ab85d7c82018b7063150b1a0e861f5e683
Chris Scoffield <cscoffield@system.example.com>
2020-09-23 22:29:38 +0100

    Add declarations.go

---
+++ declarations.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	x := 42 // Use := for first declaration and assignment
+	fmt.Println(x)
+	x = 99 // No need for the colon as already been decalred
+	fmt.Println(x)
+	y := 100 + 70
+	fmt.Println(y)
+	z := "Chris S"
+	fmt.Println(z)
+}
