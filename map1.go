0a1798fcb23affb01682165543b5afce4afb76fc
Chris Scoffield <chrisdscoffield@gmail.com>
2020-10-07 16:16:51 +0100

    Add map1

---
+++ map1.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	m := map[string]int{
+		"James":           32,
+		"Miss Moneypenny": 27,
+	}
+	fmt.Println(m)
+	fmt.Println(m["James"])
+
+	// We get a zero value back if bad key
+	// but this might be a problem
+
+	fmt.Println(m["Scoffers"])
+
+	// So use comma ok idiom to check
+	v, ok := m["Scoffers"]
+	fmt.Println(v)
+	fmt.Println(ok)
+
+	// So on a one-er
+	if v, ok := m["Scoffers"]; ok {
+		fmt.Println("This is it:", v)
+	} else {
+		fmt.Println("Nope!")
+	}
+
+	// Now add an element
+	m["Scoffers"] = 53
+	if v, ok := m["Scoffers"]; ok {
+		fmt.Println("This is it:", v)
+	} else {
+		fmt.Println("Nope!")
+	}
+
+	// Range over te map
+	for key,val := range m {
+		fmt.Println(val,key)
+	}
+
+}
